79c72f72349159989c6952d6df8a9f08b1066d3b
harsh2000yadav <harsh2000yadav@gmail.com>
2019-10-02 17:25:06 +0530

    linearSearch Added

--- linearSearch.java
+++ linearSearch.java
@@ -1,2 +1,34 @@
-test
-g
+import java.util.Scanner;
+
+ class function{	
+	
+	public static int LinearSearch(int[] arr, int num){
+		for(int i=0; i<arr.length; i++){
+            if(arr[i] == num)
+                return i;
+        }
+        
+        return -1;
+    }
+    
+    
+}
+
+
+public class linearSearch {
+	static Scanner s = new Scanner(System.in);
+	public static int[] takeInput() {
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = takeInput();
+		int num = s.nextInt();
+		System.out.print(function.LinearSearch(arr, num));
+	}
+}
